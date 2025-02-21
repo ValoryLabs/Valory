f7aaf2c02c3c0b039027f1c0482a20dac74c8b6b
Misha Gusev <majikx.prod0@gmail.com>
2025-02-22 02:47:47 +0500

    feat: static

--- frontend_v2/components/MainPage/Main.vue
+++ frontend_v2/components/MainPage/Main.vue
@@ -75,7 +75,7 @@
       before:blur-[200px]
       before:-z-10"
     >
-      <NuxtImg src="/images/ValoryLogo3D.webp" height="321" alt="Valory 3D"/>
+      <img src="/images/ValoryLogo3D.webp" height="321" alt="Valory 3D"/>
     </div>
   </section>
 </template>
