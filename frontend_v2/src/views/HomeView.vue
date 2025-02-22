d74e6a8268ff1380a97d1818600d6de032fd6ad0
Misha Gusev <majikx.prod0@gmail.com>
2025-02-22 16:29:06 +0500

    feat: little adaptive for mobile

--- frontend_v2/src/views/HomeView.vue
+++ frontend_v2/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { defineAsyncComponent } from 'vue'
 import Header from '@/components/ui/Header.vue'
 import Main from '@/components/HomePage/Main.vue'
+import { hidden } from "@/utils";
 
 const Features = defineAsyncComponent(() => import('@/components/HomePage/Features.vue'))
 const Streamers = defineAsyncComponent(() => import('@/components/HomePage/Streamers.vue'))
@@ -15,7 +16,7 @@ const Footer = defineAsyncComponent(() => import('@/components/ui/Footer.vue'))
     <div class="flex flex-col gap-16">
       <Main />
       <Features />
-      <Streamers />
+      <Streamers v-if="!hidden"/>
       <OpenSource />
     </div>
   </div>
