ea13d99b9aa4abc96e3d28c0f8e43e91f51c62e4
Misha Gusev <majikx.prod0@gmail.com>
2025-02-22 15:29:03 +0500

    refactor: prettier

--- frontend_v2/src/composables/useConfetti.ts
+++ frontend_v2/src/composables/useConfetti.ts
@@ -1,8 +1,3 @@
-/**
- * Easily add confetti effects to your Nuxt application
- *
- * @see https://github.com/catdad/canvas-confetti
- */
 import useConfetti from 'canvas-confetti'
 
 export default useConfetti
