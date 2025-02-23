f9f7582b99b37c8724d757d7bd65f1c529df3e2d
Misha Gusev <majikx.prod0@gmail.com>
2025-02-23 19:29:45 +0500

    chore: add preconnect for google fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,6 +19,11 @@ useHead({
   title: metaTitle,
   titleTemplate: (title) => (title === metaTitle ? title : `${title} - ${titleMain}`),
   meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'use-credentials' },
+    { rel: 'preload', as: 'style', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap' },
+  ],
 })
 
 useSeoMeta({
