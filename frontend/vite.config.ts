a7a4850015e395abd6fa73ae1752f04d6c936e7a
Misha Gusev <majikx.prod0@gmail.com>
2025-02-28 06:19:06 +0500

    fix(dashboard): rollup options

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import path from "node:path";
+import autoprefixer from 'autoprefixer'
+import path from 'node:path'
+import { URL, fileURLToPath } from 'node:url'
+import tailwind from 'tailwindcss'
+import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
-import webfontDownload from 'vite-plugin-webfont-dl';
+import webfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,7 +24,9 @@ export default defineConfig({
       module: 'petite-vue-i18n',
       include: [path.resolve(__dirname, './src/locales/**')],
     }),
-    webfontDownload(['https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Russo+One&display=swap']),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Russo+One&display=swap',
+    ]),
   ],
   clearScreen: false,
   resolve: {
@@ -36,14 +37,14 @@ export default defineConfig({
   build: {
     target: 'esnext',
     sourcemap: false,
-  },
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        vue: ['vue'],
-        'vue-router': ['vue-router'],
-        'vue-i18n': ['vue-i18n'],
-        pinia: ['pinia'],
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          'vue-router': ['vue-router'],
+          'vue-i18n': ['vue-i18n'],
+          pinia: ['pinia'],
+        },
       },
     },
   },
