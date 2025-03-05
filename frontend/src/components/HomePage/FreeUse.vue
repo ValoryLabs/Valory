8064479d164c007c7b111a18e570b0eca6034bbe
Misha Gusev <majikx.prod0@gmail.com>
2025-03-05 23:38:01 +0500

    feat(frontend): add localization for free use section

--- frontend/src/components/HomePage/FreeUse.vue
+++ frontend/src/components/HomePage/FreeUse.vue
@@ -7,12 +7,11 @@ import InteractiveHoverButton from '@/components/ui/InteractiveHoverButton.vue'
     <div
       class="bg-white/2 container relative z-10 flex flex-col items-center gap-8 overflow-hidden rounded-2xl border border-white/20 px-12 py-24 backdrop-blur-[8px]"
     >
-      <span class="text-center text-4xl font-bold"> Are you ready? </span>
-      <span class="text-center text-xl font-normal">
-        Join thousands of content creators who have improved the quality
-        <br />of their broadcasts with using our tools.
-      </span>
-      <InteractiveHoverButton text="Let's start!" size="lg" class="w-[11rem] text-base" />
+      <span class="text-center text-4xl font-bold">{{ $t('freeuse.title') }}</span>
+      <span class="whitespace-pre-line text-center text-xl font-normal">{{
+        $t('freeuse.description')
+      }}</span>
+      <InteractiveHoverButton :text="$t('freeuse.button')" size="lg" class="w-[11rem] text-base" />
       <div
         class="bg-gradient absolute -top-96 left-0 right-0 -z-10 rounded-full p-52 blur-[100px]"
       ></div>
