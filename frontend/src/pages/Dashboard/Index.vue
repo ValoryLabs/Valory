31ce1da4e71ba0f165a793c7f2cd2ce91d9dd08a
Misha Gusev <majikx.prod0@gmail.com>
2025-03-22 01:10:10 +0500

    feat(frontend): add dynamic greeting based on time of day in Index.vue

--- frontend/src/pages/Dashboard/Index.vue
+++ frontend/src/pages/Dashboard/Index.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 useHead({
   title: 'Dashboard',
+})
+const currentHour = new Date().getHours()
+
+const greeting = computed(() => {
+  if (currentHour >= 5 && currentHour < 12) {
+    return 'Good Morning'
+  } else if (currentHour >= 12 && currentHour < 18) {
+    return 'Good Afternoon'
+  } else if (currentHour >= 18 && currentHour < 22) {
+    return 'Good Evening'
+  } else {
+    return 'Good Night'
+  }
 })
 
 const username = ref('MAGICXcmd')
@@ -12,7 +25,7 @@ const username = ref('MAGICXcmd')
 <template>
   <section class="container">
     <div class="flex flex-col gap-1">
-      <span class="text-2xl font-bold">Good Morning, {{ username }}</span>
+      <span class="text-2xl font-bold">{{ greeting }}, {{ username }}</span>
       <span class="text-base font-medium">Here's a quick 7-day overview of your channel.</span>
     </div>
   </section>
