065495350d25ed282d4991d8cfb93de15efa61cb
Misha Gusev <majikx.prod0@gmail.com>
2025-03-23 01:12:01 +0500

    feat: Add Highlights component to DashboardPage

---
+++ frontend/src/components/DashboardPage/Highlights.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="pointer-events-none fixed left-0 top-0 z-20 h-64 w-3/4 -translate-y-[175%] -rotate-[8deg] rounded-bl-[20px] rounded-br-[120px] rounded-tl-[120px] rounded-tr-[20px] opacity-5 shadow-[20px_200px_240px_rgb(189,0,0)]"
+  ></div>
+  <div
+    class="pointer-events-none fixed bottom-0 left-auto right-[5%] top-auto z-20 h-64 w-3/4 translate-y-[200%] rotate-180 rounded-bl-[20px] rounded-br-[120px] rounded-tl-[120px] rounded-tr-[20px] opacity-5 shadow-[20px_200px_240px_rgb(189,0,0)]"
+  ></div>
+</template>
--- frontend/src/layouts/Dashboard.vue
+++ frontend/src/layouts/Dashboard.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/DashboardPage/AppSidebar.vue'
 import Header from '@/components/DashboardPage/Header.vue'
+// import Highlights from '@/components/DashboardPage/Highlights.vue'
 import { SidebarProvider } from '@/components/ui/sidebar'
 </script>
 
 <template>
   <div class="flex flex-col">
+    <!-- <Highlights /> -->
     <Header />
     <SidebarProvider class="mt-14 flex flex-row">
       <AppSidebar />
